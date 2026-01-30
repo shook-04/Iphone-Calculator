a7b9bdd4e4f59550a66a461668d88b4ef14c7cc9
shook-04 <nawfat04@gmail.com>
2026-01-30 19:35:52 +0600

    Update clock

---
+++ app.js
@@ -0,0 +1,24 @@
+//DOM Elements
+const hourEl = document.querySelector('.hour');
+const minuteEl = document.querySelector('.minute');
+
+
+
+
+
+//Set up the time
+const updateTime = () => {
+    const currentTime = new Date();
+
+    let currentHour = currentTime.getHours();
+    const currentMinute = currentTime.getMinutes();
+
+    if(currentHour > 12) {
+        currentHour -= 12;
+    }
+
+    hourEl.textContent = currentHour.toString();
+    minuteEl.textContent = currentMinute.toString().padStart(2, '0');
+}
+setInterval(updateTime, 1000);
+updateTime();
